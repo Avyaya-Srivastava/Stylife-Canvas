841317a26a1ee8e2a87359e34f0891f28c91eba6
Avyaya Srivastava <avyaya.4@gmail.com>
2024-09-26 22:45:41 +0530

    Working On Home Page

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import React from 'react';
 
 const Customizer = () => {
-  return (
-    <div>Customizer</div>
-  )
+    return (
+        <div>Customizer</div>
+    )
 }
 
 export default Customizer
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,8 +1,52 @@
-import React from 'react'
+import { motion, AnimatePresence } from 'framer-motion';
+import { useSnapshot } from 'valtio';
+
+import state from '../store';
+import { CustomButton } from '../components';
+import { 
+  headContainerAnimation,
+  headContentAnimation,
+  headTextAnimation,
+  slideAnimation
+} from '../config/motion';
 
 const Home = () => {
+  const snap = useSnapshot(state);
+
   return (
-    <div>Home</div>
+   <AnimatePresence>
+    {snap.intro &&(
+      <motion.section className='home' {...slideAnimation('left')}>
+        <motion.header {...slideAnimation("down")}>
+          <img
+            src='./threejs.png'
+            alt='logo'
+            className='w-8 h-8 object-contain'
+          />
+        </motion.header>
+
+        <motion.div className='home-content' {...headContainerAnimation}>
+            <motion.div {...headTextAnimation}>
+              <h1 className='head-text'>
+                LET'S <br className='x1:block hidden'/> STYLIFE.
+              </h1>
+            </motion.div>
+        </motion.div>
+        <motion.div {...headContentAnimation} className='flex flex-col gap-5'>
+          <p className='max-w-md font-normal text-grey-600 text-base'>
+            Put your Imagination on your Cloth, with our brand-new 3D customization tool.<strong>Unleash your Imagination</strong>{" "}and define your own Style.
+          </p>
+
+          <CustomButton
+            type="filled"
+            title= "Customize It"
+            handleClick={() => state.intro = false}
+            customeStyles="w-fit px-4 py-2.5 font-bold text-sm"
+          />
+        </motion.div>
+      </motion.section>
+    )}
+   </AnimatePresence>
   )
 }
 
